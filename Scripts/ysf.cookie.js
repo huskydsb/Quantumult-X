41543350455ec7f505edd346f15d3c7c366fb5b3
huskydsb <huskydsb@outlook.com>
2024-10-29 23:05:54 +0800

    1

---
+++ Scripts/ysf.cookie.js
@@ -0,0 +1,35 @@
+const cookieKey = 'unipay_cookieKey';
+const authorizationKey = 'unipay_authorizationKey';
+const userAgentKey = 'unipay_userAgentKey';
+const $tool = tool();
+
+try {
+    console.log("🍎 云闪付签到脚本获取cookie!");
+
+    let isGetCookie = typeof $request !== 'undefined' && $request.method !== 'OPTIONS';
+
+    if (isGetCookie && $request.url.indexOf("https://youhui.95516.com/newsign/api/daily_sign_in") > -1) {
+        var authorizationVal = $request.headers["Authorization"];
+        var cookieVal = $request.headers['Cookie'];
+        var userAgentVal = $request.headers['User-Agent'];
+
+        if (authorizationVal) {
+            $tool.setkeyval(authorizationVal, authorizationKey);
+            $tool.setkeyval(cookieVal, cookieKey);
+            $tool.setkeyval(userAgentVal, userAgentKey);
+            console.log("🍎 Authorization:", authorizationVal);
+            console.log("🍎 Cookie:", cookieVal);
+            console.log("🍎 User-Agent:", userAgentVal);
+            $done({});
+        }
+    } else {
+        console.log("🍎 云闪付签到脚本获取cookie失败");
+        $done({});
+    }
+
+} catch (e) {
+    console.log("🍎 错误:", e);
+    $done();
+}
+
+
