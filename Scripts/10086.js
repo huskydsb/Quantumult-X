f614f0941fc790496a3732e16c137f551e19346d
huskydsb <huskydsb@outlook.com>
2024-10-31 03:55:22 +0800

    更新 10086.js

--- Scripts/10086.js
+++ Scripts/10086.js
@@ -15,5 +15,5 @@
 hostname = clientaccess.10086.cn
 
 [rewrite_local]
-^https:\/\/clientaccess.10086.cn\/biz-orange\/LN\/uamrandcodelogin\/autoLogin url script-request-body https://raw.githubusercontent.com/chavyleung/scripts/master/10086/10086.fee.cookie.js
-^https:\/\/clientaccess.10086.cn\/biz-orange\/BN\/realFeeQuery\/getRealFee url script-request-body https://raw.githubusercontent.com/chavyleung/scripts/master/10086/10086.fee.cookie.js
+^https:\/\/clientaccess.10086.cn\/biz-orange\/LN\/uamrandcodelogin\/autoLogin url script-request-body https://raw.githubusercontent.com/yxgsir/chavyleung/master/10086/10086.cookie.js
+^https:\/\/clientaccess.10086.cn\/biz-orange\/BN\/realFeeQuery\/getRealFee url script-request-body https://raw.githubusercontent.com/yxgsir/chavyleung/master/10086/10086.cookie.js
