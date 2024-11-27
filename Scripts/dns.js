ade75c1f79b9a7fbae00dfa3da9c0dc6e510cbd2
huskydsb <huskydsb@outlook.com>
2024-11-28 00:07:24 +0800

    upload

--- Scripts/dns.js
+++ Scripts/dns.js
@@ -4,12 +4,12 @@ const message = {
 
 $configuration.sendMessage(message).then(resolve => {
     if (resolve.error) {
-        console.log("清除 DNS 缓存失败:", resolve.error);
+        console.log("DNS 缓存清除失败:", resolve.error);
     } else {
-        console.log("DNS 缓存清除成功:", JSON.stringify(resolve.ret));
+        console.log("DNS 缓存清除成功", JSON.stringify(resolve.ret));
     }
     $done();
 }, reject => {
-    console.log("出现了意外错误:", reject);
+    console.log("DNS 缓存清除出现了意外错误", reject);
     $done();
 });
